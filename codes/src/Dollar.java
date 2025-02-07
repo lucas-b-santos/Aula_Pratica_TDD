a668b8f7d26faa340e6b52d0c171a809f40ff748
lucas-b-santos <batistalucas451@gmail.com>
2025-02-06 23:06:20 -0300

    adicionada fábrca de Francos

--- codes/src/Dollar.java
+++ codes/src/Dollar.java
@@ -7,9 +7,9 @@ public class Dollar extends Money {
         this.amount = amount;
     }
 
-    public Dollar times(int multiplier) {
+    public Money times(int multiplier)  {
         return new Dollar(amount * multiplier);
-    }
+     }	
 
     public boolean equals(Object object) {
         Dollar dollar = (Dollar) object;
--- codes/src/Money.java
+++ codes/src/Money.java
@@ -1,10 +1,21 @@
 package src;
 
-public class Money {
+abstract public class Money {
     protected int amount;
-   
-    public boolean equals(Object object)  {
-       Money money = (Money) object;
-       return amount == money.amount;
-    }   
+
+    static public Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static public Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    abstract public Money times(int multiplier);
+
 }
